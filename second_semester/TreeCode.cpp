215155f218a42c99d949d4786403be03b2bdb75b
MaxSprog <maxxpankov@gmail.com>
2021-05-06 13:07:07 +0700

    tests

--- second_semester/TreeCode.cpp
+++ second_semester/TreeCode.cpp
@@ -2,11 +2,12 @@
 
 int main(){
     BinTree tree;
-    tree.add("4");
-    tree.add("*");
-    tree.add("5");
-    tree.add("/");
-    tree.add("10");
+    tree.add("a");
+    tree.add("t");
+    tree.add("b");
+    tree.add("g");
+    tree.add("c");
+    tree.add("z");
     tree.print_tree();
     cout << "----------" << endl;
     tree.print_prefix();
