595b777922f66f770c1516aefe32ebebddedf80d
MaxSprog <maxxpankov@gmail.com>
2021-10-23 13:49:37 +0700

    26fin

--- Final/26-demo.cpp
+++ Final/26-demo.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-#include <fstream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
+    int S, N;
+    cin >> S >> N;
+    vector<int> arr(N);
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    int p = arr[0], i = 1;
+    for(; i < N; i++){
+        p += arr[i];
+        if(p > S){
+            cout << i << " " << arr[i];
+            return 0;
+        }
+    }
 }
